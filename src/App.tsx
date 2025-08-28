772469dafe1aac4fd5ccf2418cb5ce3e36ed25a4
EfrainArenasCBQA <efrain.arenas@cbqasolutions.com>
2025-08-28 12:41:32 -0600

    chore: add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+import PetCard from './components/PetCard'
+
+export default function App(){
+  return (
+    <main>
+      <h1>POC Pethouse 2</h1>
+      <PetCard />
+    </main>
+  )
+}
